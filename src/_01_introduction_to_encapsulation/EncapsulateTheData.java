a93135da8c4633fb01f8eea702fac9450c2f06a7
Dexter H <dexhaehnic@gmail.com>
2020-04-27 18:30:54 -0700

    Encapsulation, Polymorphism(IP)

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -23,12 +23,70 @@ public class EncapsulateTheData {
 	//2. Create a new JUnit Test case and write tests to verify that 
 	//   the member variables' getters and setters are working
 	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
+	private int itemsReceived; //must not be negative. All negative arguments get set to 0.
+	private float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
+	private String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
+	private Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
 	
 	public static void main(String[] args) {
 		
+	}
+	
+	//
+	
+	public int getItemsReceived() {
+		return itemsReceived;
+	}
+	
+	public void setItemsReceived(int i) {
+		if(i < 0) {
+			itemsReceived = 0;
+		}else {
+			itemsReceived = i;
+		}
+	}
+	
+	//
+	
+	public float getDegreesTurned() {
+		return degreesTurned;
+	}
+	
+	public void setDegreesTurned(float d) {
+		if(d > 360.0) {
+			degreesTurned = 360;
+		}else if(d < 0.0) {
+			degreesTurned = 0;
+		}else {
+			degreesTurned = d;
+		}
+	}
+	
+	//
+	
+	public String getNomenclature() {
+		return nomenclature;
+	}
+	
+	public void setNomenclature(String s) {
+		if(s.equals("")) {
+			nomenclature = " ";
+		}else {
+			nomenclature = s;
+		}
+	}
+	
+	//
+	
+	public Object getMemeberObj() {
+		return memberObj;
+	}
+	
+	public void setMemberObj(Object o) {
+		if(o.getClass().equals(String.class)) {
+			memberObj = new Object();
+		}else {
+			memberObj = o;
+		}
 	}
 }
--- src/_03_polymorphs/BluePolymorph.java
+++ src/_03_polymorphs/BluePolymorph.java
@@ -5,14 +5,26 @@ import java.awt.Graphics;
 
 public class BluePolymorph extends Polymorph{
 
-	BluePolymorph(int x, int y) {
+	private int height = 50;
+	private int width = 50;
+	
+	
+	BluePolymorph(int x, int y, int width, int height) {
 		super(x, y);
 	}
 
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(Color.blue);
-		g.fillRect(x, y, 50, 50);
+		g.fillRect(getX(), getY(), width, height);
 	}
 	
 }
